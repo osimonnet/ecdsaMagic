d6c4c0ab831ea2c9219b6f72cde5ed265c43ae26
Oliver Simonnet <oliversimonnet@gmail.com>
2022-07-05 16:44:30 +0100

    Fixed bugs
    Fixed bugs

--- ecdsaMagic.py
+++ ecdsaMagic.py
@@ -17,7 +17,7 @@ def byteStrToInt(byteStr):
 # Return int value of message
 def getMsgValue(msg, fmt):
     if fmt == "base64":
-        data = base64.b64decode(msg)
+        data = base64.b64decode(msg).encode()
         return int(hashlib.sha256(data).hexdigest(), 16)
 
     return int(msg, 16)
@@ -34,7 +34,7 @@ def getKey(r, s1 ,s2, m1, m2, n, curve):
         d = (k*s1 - m1) * inverse_mod(r, n) % n
         
         private_key = ecdsa.SigningKey.from_secret_exponent(d, curve)
-        print(formatKey(private_key, "pem"))
+        #print(formatKey(private_key, "pem"))
 
         if private_key.get_verifying_key().pubkey.verifies(m1, Sig(s1, r)):
             return (d, k)
@@ -53,7 +53,8 @@ def getCurve(signature):
     curves = {48:ecdsa.NIST192p, 56:ecdsa.NIST224p, 
     64:ecdsa.SECP256k1, 96:ecdsa.NIST384p, 132:ecdsa.NIST521p}
 
-    return curves[len(base64.b64decode(signature))]
+    return curves[len(signature)]
+    #return curves[len(base64.b64decode(signature))]
 
 # Strip file extensions from output name ad apends new extansion
 def setOutName(outName, frmt):
@@ -99,39 +100,39 @@ def main():
         print(error); exit(1)
 
     # Try to parse argument string values to valid numerical values
-    try:
+    #try:
 
-        r_raw  = args.s[0][:int(len(args.s[0])/2) ]
-        s1_raw = args.s[0][ int(len(args.s[0])/2):]
-        s2_raw = args.s[1][ int(len(args.s[0])/2):]
+    r_raw  = args.s[0].split("|")[0]
+    s1_raw = args.s[0].split("|")[1]
+    s2_raw = args.s[1].split("|")[1]
 
-        m1 = getMsgValue(args.m[0], args.mf)
-        m2 = getMsgValue(args.m[1], args.mf)
-        r  = getSigValue(r_raw)
-        s1 = getSigValue(s1_raw)
-        s2 = getSigValue(s2_raw)
+    m1 = getMsgValue(args.m[0], args.mf)
+    m2 = getMsgValue(args.m[1], args.mf)
+    r  = getSigValue(r_raw)
+    s1 = getSigValue(s1_raw)
+    s2 = getSigValue(s2_raw)
 
-        print()
-        print("m1 ", (m1))
-        print("m2 ", (m2))
-        print(" r ", (r))
-        print("s1 ", (s1))
-        print("s2 ", (s2))
-        print()
+    print()
+    print("m1 ", (m1))
+    print("m2 ", (m2))
+    print(" r ", (r))
+    print("s1 ", (s1))
+    print("s2 ", (s2))
+    print()
 
-        curve  = getCurve(args.s[0])
-        n      = curve.order
-        d, k   = getKey(r, s1 ,s2, m1, m2, n, curve)
+    curve  = getCurve(r_raw)
+    n      = curve.order
+    d, k   = getKey(r, s1 ,s2, m1, m2, n, curve)
 
-        if d is None: fail()
+    if d is None: fail()
 
-        if args.f == "raw": key = d
-        else: key = formatKey(ecdsa.SigningKey.from_secret_exponent(d, curve), args.f)
+    if args.f == "raw": key = d
+    else: key = formatKey(ecdsa.SigningKey.from_secret_exponent(d, curve), args.f)
 
     # Mathematical error thrown
-    except Exception as e:
-        print(e)
-        fail()
+    #except Exception as e:
+    #    print(e)
+    #    fail()
 
     # If output name provided, save to file.  
     if args.o != None:
@@ -146,3 +147,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
